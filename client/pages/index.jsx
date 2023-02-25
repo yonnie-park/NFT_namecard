2438d8b54b462196ac518e479af29bbdf4c08d17
yonnie-park <juon0805@gmail.com>
2023-02-25 21:37:15 +0900

    add: routing

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <h1>jungyeon</h1>
+    <div className="main">
+      <img className="main__yonnie" src="/images/yonnie.jpg"/>
+      <h1>Jung Yeon Park</h1>
+      <div className="main__info">
+        <div className="main__infodiv">
+          <img className="main__icon" src="/images/telephone.png"/>
+          <p>010-9705-3269</p>
+        </div>
+        <div className="main__infodiv">
+          <img className="main__icon" src="/images/mail.png"/>
+          <p>juon0805@gmail.com</p>
+        </div>
+        <div className="main__infodiv">
+          <img className="main__icon" src="/images/telegram.png"/>
+          <p>@yonnieO_O</p>
+        </div>
+        <div className="main__infodiv">
+          <img className="main__icon" src="/images/twitter.png"/>
+          <p>@0xO_Oi</p>
+        </div>
+        <div className="main__infodiv">
+          <img className="main__icon" src="/images/github.png"/>
+          <p>github.com/yonnie-park</p>
+        </div>
+      </div>
+
+      <Link href="/nft">
+        <button className="main__button">
+         view NFTs 
+         </button>
+      </Link>
+    </div>
   );
 }
--- client/pages/nft/index.jsx
+++ client/pages/nft/index.jsx
@@ -4,13 +4,18 @@ export default function NFT(){
         <div className="NFTmain">
             <h1>Explore</h1>
             <div className="NFTmain__container">
+                <Link href="/nft/padding_a"><img className="NFTmain__nft" src="/images/padding_a.jpg"/></Link>
+                <Link href="/nft/padding_b"><img className="NFTmain__nft" src="/images/padding_b.jpg"/></Link>
                 <Link href="/nft/hoodie_a"><img className="NFTmain__nft" src="/images/hoodie_a.jpg"/></Link>
                 <Link href="/nft/hoodie_b"><img className="NFTmain__nft" src="/images/hoodie_b.jpg"/></Link>
                 <Link href="/nft/flower_a"><img className="NFTmain__nft" src="/images/flower_a.jpg"/></Link>
                 <Link href="/nft/flower_b"><img className="NFTmain__nft" src="/images/flower_b.jpg"/></Link>
-                <Link href="/nft/padding_a"><img className="NFTmain__nft" src="/images/padding_a.jpg"/></Link>
-                <Link href="/nft/padding_b"><img className="NFTmain__nft" src="/images/padding_b.jpg"/></Link>
             </div>
+            <Link href="/">
+                <button className="main__button">
+                    go to info 
+                </button>
+            </Link>
         </div>
     )
 }
